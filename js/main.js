4a123528ddd796e26490876caeb62f847e6d1e69
Pavel Ksonofontov <j.a.kameas@gmail.com>
2015-09-04 18:53:30 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -210,7 +210,9 @@ $(document).ready(function(){
     variableWidth: true,
     focusOnSelect: true,
     arrows: false,
-    asNavFor: '.b-gallery_placeholders'
+    asNavFor: '.b-gallery_placeholders',
+    infinite: true,
+    slidesToShow: 10
   });
   $('.m-slider-price').slick({
     slidesToShow: 1,
@@ -398,6 +400,12 @@ $(document).ready(function(){
     }
 
   })
+
+  if ($('.s-scrollbar').length) {
+    $(".s-scrollbar").mCustomScrollbar({
+      scrollInertia: 200
+    });
+  };
 });
 
 
@@ -516,7 +524,7 @@ function FloatPanel() {
           top: 0,
           height: heightBlock
         });
-        console.log('here')
+        // console.log('here')
       } else if ($(window).scrollTop() > endBlock) {
         $('.b-float_panel').css({
           top: stopBlock
@@ -1120,4 +1128,24 @@ function CustomMap() {
       });
     }
   }
+}
+
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    var mapOptions = {
+        zoom: 17,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        center: new google.maps.LatLng(48.484197,135.0854547),
+        styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+    };
+    var mapElement = document.getElementById('b-arena_map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(48.484197,135.0854547),
+        map: map,
+        icon: './img/marker.png'
+    });
 }
